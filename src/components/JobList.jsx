d81cce3a97f414ad0900c46cc24480acb2bb20f9
Himanshu Kashyap <38201652+himanshuxgit@users.noreply.github.com>
2024-05-01 12:53:31 +0530

    Add loader

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,45 +1,38 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, CircularProgress, Typography } from "@mui/material";
 
 const JobsList = () => {
   const [jobDetails, setJobDetails] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
   const pageRef = useRef(0);
-
   useEffect(() => {
     fetchJobDetails();
   }, []);
-
   const fetchJobDetails = async () => {
     if (!hasMore || isLoading) return;
-
     setIsLoading(true);
-
     try {
       const myHeaders = new Headers();
       myHeaders.append("Content-Type", "application/json");
 
       const limit = 10; // Number of results per page
-      const offset = (pageRef.current) * limit; // Calculate offset for pagination
+      const offset = pageRef.current * limit; // Calculate offset for pagination
 
       const raw = JSON.stringify({
         limit: limit,
         offset: offset,
       });
-
       const requestOptions = {
         method: "POST",
         headers: myHeaders,
         body: raw,
       };
-
       const response = await fetch(
         "https://api.weekday.technology/adhoc/getSampleJdJSON",
         requestOptions
       );
       const data = await response.json();
-
       // Check if there are more results
       if (data.jdList.length === 0) {
         setHasMore(false);
@@ -48,12 +41,11 @@ const JobsList = () => {
         pageRef.current++;
       }
     } catch (error) {
-      console.error("Error fetching job details:", error);
+      console.error("Error fetching job details: ", error);
     } finally {
       setIsLoading(false);
     }
   };
-
   const handleScroll = () => {
     if (
       document.documentElement.offsetHeight -
@@ -62,8 +54,9 @@ const JobsList = () => {
       isLoading
     ) {
       return;
+    } else {
+      fetchJobDetails();
     }
-    fetchJobDetails();
   };
 
   useEffect(() => {
@@ -72,32 +65,39 @@ const JobsList = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-8">
-      {jobDetails.map((job, index) => (
-        <Card key={index} className="w-11/12">
-          <CardContent className="bg-emerald-100">
-            <Typography variant="h5" component="div">
-              {job.jobRole}
-            </Typography>
-            <Typography color="textSecondary" gutterBottom>
-              {job.location}
-            </Typography>
-            <Typography variant="body2" component="p">
-              {job.jobDetailsFromCompany}
-            </Typography>
-            <Typography color="textSecondary" gutterBottom>
-              Salary: {job.minJdSalary} - {job.maxJdSalary}{" "}
-              {job.salaryCurrencyCode}
-            </Typography>
-            <Typography color="textSecondary">
-              Experience: {job.minExp} - {job.maxExp} years
-            </Typography>
-          </CardContent>
-        </Card>
-      ))}
-      {isLoading && <div>Loading...</div>}
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-8">
+        {jobDetails.map((job, index) => (
+          <Card key={index} className="w-11/12">
+            <CardContent className="bg-emerald-100">
+              <Typography variant="h5" component="div">
+                {job.jobRole}
+              </Typography>
+              <Typography color="textSecondary" gutterBottom>
+                {job.location}
+              </Typography>
+              <Typography variant="body2" component="p">
+                {job.jobDetailsFromCompany}
+              </Typography>
+              <Typography color="textSecondary" gutterBottom>
+                Salary: {job.minJdSalary} - {job.maxJdSalary}{" "}
+                {job.salaryCurrencyCode}
+              </Typography>
+              <Typography color="textSecondary">
+                Experience: {job.minExp} - {job.maxExp} years
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      {isLoading && (
+        <div className="mt-8 flex justify-center">
+          <CircularProgress />
+        </div>
+      )}
       {!isLoading && !hasMore && <div>No more jobs</div>}
-    </div>
+    </>
   );
 };
+
 export default JobsList;
