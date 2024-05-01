8b4816487a992e70c11214d3cb80766d9c229481
Himanshu Kashyap <38201652+himanshuxgit@users.noreply.github.com>
2024-05-01 10:16:10 +0530

    Render component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import "./App.css";
+import JobsList from "./components/JobList";
 
 function App() {
-  return <></>;}
-  export default App;
+  return (
+    <>
+      <div className="container mx-auto py-8 ">
+        <h1 className="text-white text-3xl font-bold mb-4">Job Listings</h1>
+        <JobsList />
+      </div>
+    </>
+  );
+}
+
+export default App;
